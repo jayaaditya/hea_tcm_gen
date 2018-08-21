de1e4078b721974391c570ba5b35177d6db01ad3
Jaya Aditya <jayaaditya@outlook.com>
2018-08-21 22:32:35 +0530

    add macro new

--- generators/new_FeCo.py
+++ generators/new_FeCo.py
@@ -12,9 +12,9 @@ out
 """
 
 print_str="""s-co T=%d,n=1,p=1e5
-s-co x(Co)=0.2,x(Cu)=0.2,x(Ti)=0.2,x(Ni)=0.2,x(Fe)=0.2;
+s-co x(Co)=0.2,x(Cu)=0.2,x(Ti)=0.2,x(Ni)=0.2
 c-eq
-show x(Co),x(Cu),x(Ti),x(Ni),x(Fe)
+show t
 show np(c14_laves)
 show np(c15_laves)
 show np(hcp_zn)
@@ -22,8 +22,8 @@ show np(ni3ti_d024)
 show np(fcc_l12)
 show np(liquid)
 """
-with open('macro_test.tcm','w') as f:
+with open('macro.tcm','w') as f:
     f.write(init_str)
-    for x in range(500,501):
+    for x in range(500,1501):
         f.write(print_str % x)
 
